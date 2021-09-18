0a3683390a7155780c08f9bb755548d4fb8d8143
Irina <bokhanko-irina@yandex.ru>
2021-09-18 12:49:37 +0300

    feat:add burger-menu and project's examples

---
+++ index.js
@@ -0,0 +1,21 @@
+const btnTop = document.querySelector('.top');
+const burger = document.querySelector('.header__burger');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', function () {
+   burger.classList.toggle('active');
+   nav.classList.toggle('active');
+}
+)
+
+
+window.addEventListener('scroll', function () {
+   if (window.scrollY > '130') {
+      btnTop.classList.remove('hidden');
+      console.log('130')
+   } else {
+      btnTop.classList.add('hidden');
+      console.log('130')
+   }
+});
+
